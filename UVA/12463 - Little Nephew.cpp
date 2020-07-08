2edf830605bb2ee5690f48df20a160c4667e2425
ayan978 <65063367+ayan978@users.noreply.github.com>
2020-07-08 22:15:01 +0600

    Create 12463 - Little Nephew.cpp

---
+++ UVA/12463 - Little Nephew.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+int a,b,c,d,e;
+
+while(cin>>a>>b>>c>>d>>e){
+
+    if(a==0 && b==0 && c==0 && d==0 && e==0){
+        break;
+    }
+    else{
+
+        cout<<a*b*c*(d*d)*(e*e)<<endl;
+    }
+}
+}
